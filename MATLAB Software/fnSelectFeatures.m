deabcb3ae5819aeb1ad2d8f74c77c1dd2243e4c4
PaulBarron <paul.barron@attabotics.com>
2023-08-05 14:10:42 -0600

    Add automatic reducing of features

--- MATLAB Software/fnSelectFeatures.m
+++ MATLAB Software/fnSelectFeatures.m
@@ -1,6 +1,16 @@
-function [selectedFeatureArray, selectedFeatureData, numOfSelectedFeatures] = fnSelectFeatures(selectedSignal, selectedFeature, featureArray, R2, upperR2, lowerR2)
+function [selectedFeatureArrayReduced, selectedFeatureDataReduced, numOfSelectedFeaturesReduced] = fnSelectFeatures(selectedSignal, selectedFeature, featureArray, R2, upperR2, lowerR2, r2ReducedThreshold, verbose)
+%function [selectedFeatureArray, selectedFeatureData, numOfSelectedFeatures] = fnSelectFeatures(selectedSignal, selectedFeature, featureArray, R2, upperR2, lowerR2)
+%{
+Description: This function...
+Date: 2023/03/19
+Author: Paul Barron
+Inputs:     
+Outputs:    selectedFeatureArray: 
+            selectedFeatureData:
+            numOfSelectedFeatures:
+%}
     numOfCombinedFeatures = size(R2, 1);
-    selectedFeatureArray = [selectedFeature]; % Array of correlated features
+    selectedFeatureArray = []; % Array of correlated features
     for i = 1:selectedFeature-1
         if R2(i,selectedFeature) > lowerR2 && R2(i,selectedFeature) < upperR2
             selectedFeatureArray = [selectedFeatureArray i];
@@ -11,6 +21,29 @@ function [selectedFeatureArray, selectedFeatureData, numOfSelectedFeatures] = fn
             selectedFeatureArray = [selectedFeatureArray i];
         end
     end
+
+    selectedFeatureArrayReduced = selectedFeatureArray;
+    for index1 = 1 : size(selectedFeatureArrayReduced, 2)
+        duplicates = [];
+        for index2 = index1 + 1 : size(selectedFeatureArrayReduced, 2)
+            if R2(selectedFeatureArrayReduced(index1), selectedFeatureArrayReduced(index2)) > r2ReducedThreshold
+                if verbose == true
+                    fprintf("Sig %.0f Sig %.0f = %.2f", selectedFeatureArrayReduced(index1), selectedFeatureArrayReduced(index2), R2(selectedFeatureArrayReduced(index1), selectedFeatureArrayReduced(index2)));
+                    fprintf('\n');
+                end
+                duplicates = [duplicates selectedFeatureArrayReduced(index2)];
+            end
+        end
+        
+        for i = 1:size(duplicates, 2)
+            ignoreFeature = duplicates(i);
+            [tf,loc] = ismember(ignoreFeature, selectedFeatureArrayReduced);
+            selectedFeatureArrayReduced(selectedFeatureArrayReduced == ignoreFeature) = [];
+        end
+    end
+
+    % Add the original selected feature back into the array
+    selectedFeatureArray = [selectedFeatureArray selectedFeature]; 
     selectedFeatureArray = sort(selectedFeatureArray);
     numOfSelectedFeatures = size(selectedFeatureArray, 2);
     selectedFeatureData = {numOfSelectedFeatures};
@@ -18,6 +51,21 @@ function [selectedFeatureArray, selectedFeatureData, numOfSelectedFeatures] = fn
     for i = 1:numOfSelectedFeatures
         selectedFeatureData{i} = featureArray(:, selectedFeatureArray(i), selectedSignal);
     end
+
+    if verbose == true 
+        disp(selectedFeatureArray);
+    end
+
+    selectedFeatureArrayReduced = [selectedFeatureArrayReduced selectedFeature];
+    selectedFeatureArrayReduced = sort(selectedFeatureArrayReduced);
+    numOfSelectedFeaturesReduced = size(selectedFeatureArrayReduced, 2);
+    selectedFeatureDataReduced = {numOfSelectedFeaturesReduced};
+
+    for i = 1:numOfSelectedFeaturesReduced
+        selectedFeatureDataReduced{i} = featureArray(:, selectedFeatureArray(i), selectedSignal);
+    end
     
-    numOfSelectedFeatures = size(selectedFeatureArray, 2);
+    if verbose == true 
+        disp(selectedFeatureArrayReduced);
+    end
 end
